c963ea115ec4727a5a6358090a6e00fa99c9d70e
ozanshll <116595268+ozanshll@users.noreply.github.com>
2025-04-10 13:53:35 +0200

    Create python_data_types.py

---
+++ python_data_types.py
@@ -0,0 +1,46 @@
+#Data Types in Python
+
+# String
+x = "Hello World"
+
+# Integer
+x = 20
+
+# Float
+x = 20.5
+
+# Complex
+x = 1j
+
+# List
+x = ["apple", "banana", "cherry"]
+
+# Tuple
+x = ("apple", "banana", "cherry")
+
+# Range
+x = range(6)
+
+# Dictionary
+x = {"name": "John", "age": 36}
+
+# Set
+x = {"apple", "banana", "cherry"}
+
+# Frozenset
+x = frozenset({"apple", "banana", "cherry"})
+
+# Boolean
+x = True
+
+# Bytes
+x = b"Hello"
+
+# Bytearray
+x = bytearray(5)
+
+# Memoryview
+x = memoryview(bytes(5))
+
+# NoneType
+x = None
