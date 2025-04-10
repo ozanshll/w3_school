79724a2b61ae65edccfe5b202683f20fcc093e21
ozanshll <116595268+ozanshll@users.noreply.github.com>
2025-04-10 13:55:08 +0200

    Create python_variables.py

---
+++ python_variables.py
@@ -0,0 +1,36 @@
+#Creating Variables
+
+# Numbers
+age = 23                    # Integer
+height = 1.83               # Float
+
+# Text
+name = "Akin"              # String
+greeting = 'Hello, world!'  # String
+
+# Boolean
+is_hungry = True
+is_raining = False
+
+# List
+fruits = ["apple", "banana", "cherry"]
+numbers = [1, 2, 3, 4, 5]
+
+# Dictionary
+person = {
+    "name": "Akin",
+    "age": 23,
+    "city": "Mersin"
+}
+
+# Multiple assignment
+x, y, z = 1, 2, 3
+a = b = c = 0
+
+# Print types
+print(type(name))   # <class 'str'>
+print(type(age))    # <class 'int'>
+print(type(height)) # <class 'float'>
+print(type(is_hungry)) # <class 'bool'>
+print(type(fruits)) # <class 'list'>
+print(type(person)) # <class 'dict'>
